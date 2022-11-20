2526e52d1546c8b2cfb95e48c1ee7ba0f9131121
Bearei <bear_ei@outlook.com>
2022-11-20 16:48:59 +0800

    fix(Menu): Fix the problem of packaging without missing dependencies. (#6)
    * feat: version 0.0.1
    
    * feat: version 0.0.2
    
    * feat(index): add menu option export.
    
    * feat(Menu): update menu option parameters.
    
    * fix(Menu): Fix an export type conflict.
    
    * fix(Menu): Fix an export type conflict.
    
    * fix(Menu): Fix the problem of packaging without missing dependencies.

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,4 +1,5 @@
-import React, {useEffect, useState} from 'react';
+import * as React from 'react';
+import {useEffect, useState} from 'react';
 import {HandleEvent, handleEvent} from '@bearei/react-util';
 import {MenuItemProps} from './MenuItem';
 
